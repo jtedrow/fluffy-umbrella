f93b19975209b746f5a58a3c575c55b4339a8a88
James Tedrow <jamesallentedrow@gmail.com>
2019-10-06 10:49:41 -0400

    Add new files

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@app.route("/")
+def index():
+    """Return the homepage."""
+    return render_template("index.html")
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+api.add_resource(HelloWorld, '/hw')
+
+if __name__ == '__main__':
+    app.run(debug=True)
