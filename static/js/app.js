23ed826d261e52fd153e3f57c8e08e0de8e1a8f0
James Tedrow <jamesallentedrow@gmail.com>
2019-10-17 11:36:24 -0400

    Add graphs to new html layout

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,144 @@
+function init() {
+    // Grab a reference to the dropdown select element
+    var selectorWard = d3.select("#selWard");
+    var selectorYear = d3.select("#selYear");
+
+    selectorWard
+        .append("option")
+        .text("-")
+        .property("value", "-");
+
+    selectorYear
+        .append("option")
+        .text("-")
+        .property("value", "-");
+
+    // Use the list of sample names to populate the select options
+    d3.json("/names").then((d) => {
+        let wards = d[4]["Wards"]
+        let years = d[0]["SEASON YEAR"].sort()
+        wards.forEach((ward) => {
+            selectorWard
+                .append("option")
+                .text(ward)
+                .property("value", ward);
+        });
+        years.forEach((year) => {
+
+            selectorYear
+                .append("option")
+                .text(year)
+                .property("value", year);
+        });
+        // Use the first sample from the list to build the initial plots
+        buildCharts(2)
+        buildPie(2019)
+
+    });
+
+
+}
+
+function buildCharts(sample) {
+    console.log(sample);
+
+    d3.json(`/ward/${sample}`).then((data) => {
+        let years = [...new Set(data.map(x => x["SEASON YEAR"]))].sort();
+        let positives = [];
+        let negatives = [];
+
+        years.forEach(y => {
+            let filtered = data.filter(x => x["SEASON YEAR"] === y);
+            let pos_filtered = filtered.filter(x => x.RESULT == "positive");
+            let neg_filtered = filtered.filter(x => x.RESULT == "negative");
+
+            positives.push(pos_filtered.length);
+            negatives.push(neg_filtered.length);
+        })
+
+        var trace1 = {
+            x: years,
+            y: positives,
+            name: 'Positive',
+            type: 'bar',
+            marker: {
+                color: 'rgb(15,140,121)',
+                line: {
+                    color: 'rgb(229,226,224)',
+                    width: 1
+                }
+            }
+        };
+
+        var trace2 = {
+            x: years,
+            y: negatives,
+            name: 'Negative',
+            type: 'bar',
+            marker: {
+                color: 'rgb(189,45,40)',
+                line: {
+                    color: 'rgb(229,226,224)',
+                    width: 1
+                }
+            }
+        };
+
+        var data = [trace1, trace2];
+
+        var layout2 = {
+            title: `Ward ${sample} Results by Year`,
+            barmode: 'stack',
+            plot_bgcolor: 'rgb(239,236,234)',
+            paper_bgcolor: 'rgb(239,236,234)'
+        };
+
+        Plotly.newPlot('wardBarChart', data, layout2);
+
+    })
+};
+
+function buildPie(sample) {
+    d3.json(`/year/${sample}`).then((data) => {
+        panel = d3.select("#pieChart");
+        panel.html("")
+        console.log(data)
+
+        let uniqueTypes = [...new Set(data.map(x => x["SPECIES"]))].sort();
+        let diction = []
+
+        console.log(uniqueTypes)
+
+
+        uniqueTypes.forEach(type => {
+            let dict = {
+                Type: type,
+                Count: data.filter(x => x.SPECIES == `${type}`).length
+            }
+            diction.push(dict)
+        });
+
+        console.log(diction)
+
+        let chart = [{
+            values: diction.map(x => x.Count),
+            labels: diction.map(x => x.Type),
+            type: 'pie'
+        }];
+
+        var layout = {
+            title: `Distribution by Species: ${sample}`,
+            plot_bgcolor: 'rgb(239,236,234)',
+            paper_bgcolor: 'rgb(239,236,234)'
+        }
+
+        Plotly.newPlot('pieChart', chart, layout);
+    })
+};
+
+buildPie(2019)
+init()
+
 d3.json("/summary/").then((data) => {
     year = []
     pos = []
@@ -13,7 +154,7 @@ d3.json("/summary/").then((data) => {
     var trace1 = {
         x: year,
         y: pos,
-        name: 'Positives',
+        name: 'Positive',
         type: 'bar',
         marker: {
             color: 'rgb(15,140,121)',
@@ -42,12 +183,16 @@ d3.json("/summary/").then((data) => {
 
     var layout = {
         title: "West Nile Virus Results per Year",
-        barmode: 'stack'
+        barmode: 'stack',
+        plot_bgcolor: 'rgb(239,236,234)',
+        paper_bgcolor: 'rgb(239,236,234)'
     };
 
     Plotly.newPlot('barChart', data, layout);
 
 });
+
+
 
 
 
